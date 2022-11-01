a9a23ebf4011d78719eef6e404700dd1de4f88c4
evgen <Evgeny.Grand@mail.ru>
2022-11-01 23:04:03 +0300

    Correct locators

--- src/main/java/components/fouthTask/MainMenuCalendar.java
+++ src/main/java/components/fouthTask/MainMenuCalendar.java
@@ -18,10 +18,10 @@ public class MainMenuCalendar extends AbsBasePages {
     @FindBy(xpath = "//*[contains(@class, 'header2-menu_main')]/*[contains(@class, 'header2-menu__item')][.//*[text()='События']]//a[@title='Календарь мероприятий']")
     private WebElement subMenu;
 
-    @FindBy(css = "//header/div/div/div/div/p[contains(text(),'События')]")
+    @FindBy(xpath = "//p[contains(text(), 'События')]")
     private WebElement openDropdownMenu;
 
-    @FindBy(css = "body > div.body-wrapper > div > div.js-dod-new-events-root > div > section > header > div.dod_new-events__header-left > div > div.dod_new-events-dropdown__list.js-dod_new_events-dropdown > a:nth-child(4)")
+    @FindBy(xpath = "//*[not(contains(@rel, 'nofollow')) and contains(text(),'мероприятий')]")
     private WebElement changeTypeDropdownMenu;
 
 
